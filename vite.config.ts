4f4a29c4414d3e867e6baa22575ff24d87143cb0
Muhammedijas981 <muhammedijas981@gmail.com>
2025-08-25 23:05:04 +0530

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// vite.config.ts
-const vite_1 = require("vite");
-const plugin_react_1 = require("@vitejs/plugin-react");
-const url_1 = require("url");
-const path_1 = require("path");
-// ESM __dirname shim
-const __filename = (0, url_1.fileURLToPath)(import.meta.url);
-const __dirname = (0, path_1.dirname)(__filename);
-exports.default = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_1.default)()],
-    resolve: {
-        alias: {
-            "@": (0, path_1.resolve)(__dirname, "src"),
-        },
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
 });
